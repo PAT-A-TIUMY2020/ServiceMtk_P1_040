b31361be27bf8033a057d0fbfca972927b12dd6b
Widyaputriheso <62939137+Widyaputriheso@users.noreply.github.com>
2020-11-08 17:13:15 +0700

    WCF EXCEPTION HANDLING

--- ServiceMtk_P1_040/IMatematika.cs
+++ ServiceMtk_P1_040/IMatematika.cs
@@ -7,38 +7,57 @@ using System.Text;
 
 namespace ServiceMtk_P1_040
 {
-	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-	[ServiceContract]
-	public interface IMatematika
-	{
-		[OperationContract]
-		/* output->*/
-		int Tambah(int a, int b); //method
-		[OperationContract]
-		int Kurang(int a, int b); //input
-		[OperationContract]
-		int Kali(int a, int b);
-		[OperationContract]
-		int Bagi(int a, int b);
-		[OperationContract]
-		Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
-	}
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas melambangkan exception
+        /* output->*/
+        int Tambah(int a, int b); //method
 
-	[DataContract]
-	public class Koordinat
-	{
-		private int _x, _y; //atribut
-		[DataMember]//membuka koordinat, x,y
-		public int X //properties
-		{
-			get { return _x; }
-			set { _x = value; }
-		}
-		[DataMember]
-		public int Y //properties
-		{
-			get { return _y; }
-			set { _y = value; }
-		}
-	}
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas melambangkan exception
+        int Kurang(int a, int b); //input
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas melambangkan exception
+        int Kali(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas melambangkan exception
+        int Bagi(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas melambangkan exception
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class 
+
+
+    }
+
+    [DataContract]
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x,y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
 }
